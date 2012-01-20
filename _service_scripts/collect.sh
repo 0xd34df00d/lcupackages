dc2fbcfb26fe3fa7c87320fd2c93bdd3c74c0500
0xd34df00d <0xd34df00d@gmail.com>
2012-01-20 20:37:54 +0400

    Updated Collect script.

--- _service_scripts/collect.sh
+++ _service_scripts/collect.sh
@@ -2,4 +2,4 @@
 
 d="$1/dists/main/all"
 
-for i in `/bin/ls | grep -v README | grep -v _service`; do mkdir $d/$i; cp $i/*.xml $d/$i; cp $i/arch/*.tar.gz $d/$i; done
+for i in `/bin/ls | grep -v README | grep -v _service`; do mkdir $d/$i; cp $i/*.xml $d/$i; cp $i/arch/*.tar.* $d/$i; done
